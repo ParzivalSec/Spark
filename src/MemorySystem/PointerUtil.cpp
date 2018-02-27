180bb8812d46af230b87765e9c24cce3f5982dee
ParzivalSec <lukas.vogl12@gmail.com>
2018-02-27 01:04:29 +0100

    Added IsAligned utility function

--- src/MemorySystem/PointerUtil.cpp
+++ src/MemorySystem/PointerUtil.cpp
@@ -1,22 +1,5 @@
 #include "PointerUtil.h"
 
-void* sp::pointerUtil::AlignTop(void* pointer, size_t alignment)
-{
-	PointerType ptr { pointer };
-
-	ptr.as_ptr += (alignment - 1);
-	ptr.as_ptr &= ~(alignment - 1);
-
-	return ptr.as_void;
-}
-
-void* sp::pointerUtil::AlignBottom(void* pointer, size_t alignment)
-{
-	PointerType ptr{ pointer };
-	ptr.as_ptr &= ~(alignment - 1);
-	return ptr.as_void;
-}
-
 bool sp::pointerUtil::IsPowerOfTwo(size_t number)
 {
 	// Bit pattern of POTs: 0100 => -1: 0011 => & them yields 0 for POTs
--- src/MemorySystem/PointerUtil.h
+++ src/MemorySystem/PointerUtil.h
@@ -1,35 +1,67 @@
 #pragma once
 
 #include <cstdint>
+#include <cassert>
 
 namespace sp
 {
 	namespace pointerUtil
 	{
-		///
-		/// PointerType union used for type punning
-		/// This union is an abstraction to allow a void pointer to be interpreted as a
-		/// uintptr_t without using a reinterpret cast and therefore without breaking the
-		/// pointer-aliasing rule. On should not, that using unions for type-punning is 
-		/// implemenation defined behaviour.
-		///
-		union PointerType
+		bool IsPowerOfTwo(size_t number);
+
+		template<typename T>
+		bool IsAlignedTo(T* pointer, size_t alignment)
 		{
-			void* as_void;
-			uintptr_t as_ptr;
-		};
+			union
+			{
+				uintptr_t as_uintptr;
+				T* as_T_ptr;
+			};
+
+			as_T_ptr = pointer;
+			return alignment == 0 || (as_uintptr & (alignment - 1)) == 0;
+		}
 
 		///
 		/// Align a pointer to the next higher alignment boundary (only POTs)
 		///
-		void* AlignTop(void* pointer, size_t alignment);
+		template<typename T>
+		T* AlignTop(T* pointer, size_t alignment)
+		{
+			assert(IsPowerOfTwo(alignment) && "Alignment was no power-of-two");
+
+			union
+			{
+				uintptr_t as_uintptr;
+				T* as_T_ptr;
+			};
+
+			as_T_ptr = pointer;
+			as_uintptr += (alignment - 1);
+			as_uintptr &= ~(alignment - 1);
+
+			return as_T_ptr;
+		}
 
 		///
 		/// Align the pointer to the next smaller alignment boundary (only POTs)
 		///
-		void* AlignBottom(void* pointer, size_t alignment);
+		template<typename T>
+		void* AlignBottom(void* pointer, size_t alignment)
+		{
+			assert(IsPowerOfTwo(alignment) && "Alignment was no power-of-two");
 
-		bool IsPowerOfTwo(size_t number);
+			union
+			{
+				uintptr_t as_uintptr;
+				T* as_T_ptr;
+			};
+
+			as_T_ptr = pointer;
+			as_uintptr &= ~(alignment - 1);
+
+			return as_T_ptr;
+		}
 
 		///
 		/// PointerAs
